e08c2b5d83037a920f1ac72c51894b29cfe69762
Kienan Knight-Boehm <kienan@kienankb.com>
2016-04-29 10:39:17 -0400

    Tweaked auth, added modes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var API_VERSION = 1;
-
 var config = {
-	"port" : 4242,
-	"name" : "Home",
-	"device" : "/dev/ttyACM0",
-	"presets" : [
-		["Off", [0, 0, 0]],
-		["Incandescent Hi", [255, 60, 20]],
-		["Incandescent Lo", [255, 60, 30]],
-		["Full white", [255, 255, 255]],
-		["Red", [255, 0, 0]],
-		["Green", [0, 255, 0]],
-		["Blue", [0, 0, 255]]
-	],
-	"authrequired" : false,
-	"username" : "Billy",
-	"password" : "mypass"
+    "port": 4242,
+    "name": "Home",
+    "device": "/dev/ttyACM0",
+    "presets": [
+        ["Off", [0, 0, 0]],
+        ["Incandescent Hi", [255, 60, 20]],
+        ["Incandescent Lo", [255, 60, 30]],
+        ["Full white", [255, 255, 255]],
+        ["Red", [255, 0, 0]],
+        ["Green", [0, 255, 0]],
+        ["Blue", [0, 0, 255]]
+    ],
+    "authrequired": false,
+    "username": "Billy",
+    "password": "mypass"
 };
-
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -25,113 +23,115 @@ var auth = require('basic-auth');
 var bodyParser = require('body-parser');
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
-var serial = new SerialPort (config.device,
-	{baudrate: 9600, parser: serialport.parsers.readline("\n")});
-
-var colors = { "r" : 0, "g" : 0, "b" : 0 };
+var serial = new SerialPort(config.device, { baudrate: 9600, parser: serialport.parsers.readline("\n") });
+var colors = { "r": 0, "g": 0, "b": 0 };
 var servermode = "solid";
-
 function authorized(user) {
-	if (config.authrequired) {
-		if (user == undefined) {
-			return false;
-		}
-		else {
-			return (user.name == config.username && user.pass == config.password);
-		}
-	}
-	else {
-		return true;
-	}
+    if (config.authrequired) {
+        if (user == undefined) {
+            return false;
+        }
+        else {
+            return (user.name == config.username && user.pass == config.password);
+        }
+    }
+    else {
+        return true;
+    }
 }
-
-serial.on("open", function() {
-	console.log('[INFO] Opening serial port...');
-	serial.on('data', function(data) {
-			if (data.trim() == "ok") {
-			console.log("[INFO] Ready for requests.");
-		}
-		else {
-			console.log("[ERROR] received unexpected serial data:");
-		}
-	});
+serial.on("open", function () {
+    console.log('[INFO] Opening serial port...');
+    serial.on('data', function (data) {
+        if (data.trim() == "ok") {
+            console.log("[INFO] Ready for requests.");
+        }
+        else {
+            console.log("[ERROR] received unexpected serial data:");
+        }
+    });
 });
-
-http.listen(config.port, function() {
-	console.log('[INFO] Server listening on *:' + config.port);
+http.listen(config.port, function () {
+    console.log('[INFO] Server listening on *:' + config.port);
 });
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/status/', function(req, res) {
-	var user = auth(req);
-	if (authorized(user)) {
-		var status = {
-			"version" : API_VERSION,
-			"mode" : servermode,
-			"colorsRGB" : [colors.r, colors.g, colors.b],
-			"name" : config.name,
-			"presets" : config.presets
-		};
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(status));
-		console.log("[INFO] Received status request");
-	}
-	else {
-		console.log("[INFO] Received unauthorized /status/ request");
-		res.statusCode = 401;													// Is this working?
-		res.end("Authentication required");
-	}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/status/', function (req, res) {
+    var user = auth(req);
+    if (authorized(user)) {
+        var status = {
+            "version": API_VERSION,
+            "mode": servermode,
+            "colorsRGB": [colors.r, colors.g, colors.b],
+            "name": config.name,
+            "presets": config.presets
+        };
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(status));
+        console.log("[INFO] Received status request");
+    }
+    else {
+        console.log("[INFO] Received unauthorized /status/ request");
+        res.statusCode = 401; // Is this working?
+        res.end("Authentication required");
+    }
 });
-
-app.post('/v1/update/', function(req, res) {
-	var user = auth(req);
-	if (authorized(user)) {
-		reqmode = req.body.mode;
-		if (reqmode == "jump") {
-			servermode = "solid";
-			console.log("[INFO] Jumping to "+"("+req.body.r+", "+req.body.g+", "+req.body.b+")");
-			colors.r = req.body.r;
-			colors.g = req.body.g;
-			colors.b = req.body.b;
-			sendColor(colors.r, colors.g, colors.b);
-		}
-		else if (reqmode == "fade") {
-			servermode = "solid";
-			console.log("[INFO] Fading to "+"["+req.body.r+" "+req.body.g+" "+req.body.b+"]");
-			var stepcount = 0;
-			while (colors.r != req.body.r || colors.g != req.body.g || colors.b != req.body.b) {
-				if (colors.r < req.body.r) {colors.r++;}
-				if (colors.r > req.body.r) {colors.r--;}
-				if (colors.g < req.body.g) {colors.g++;}
-				if (colors.g > req.body.g) {colors.g--;}
-				if (colors.b < req.body.b) {colors.b++;}
-				if (colors.b > req.body.b) {colors.b--;}
-				stepcount++;
-				sendColor(colors.r, colors.g, colors.b);
-			}
-			console.log("[INFO] Fade took " + stepcount + " steps");
-		}
-		else if (mode == "pulse") {
-
-		}
-		else if (mode == "blink") {
-
-		}
-		else if (mode == "disco") {
-
-		}
-		res.end();
-	}
-	else {
-		console.log("[INFO] Received unauthorized /status/ request");
-		res.statusCode = 401;													// Is this working?
-		res.end("Authentication required");
-	}
+app.post('/v1/update/', function (req, res) {
+    var user = auth(req);
+    if (authorized(user)) {
+        reqmode = req.body.mode;
+        if (reqmode == "jump") {
+            servermode = "solid";
+            console.log("[INFO] Jumping to " + "(" + req.body.r + ", " + req.body.g + ", " + req.body.b + ")");
+            colors.r = req.body.r;
+            colors.g = req.body.g;
+            colors.b = req.body.b;
+            sendColor(colors.r, colors.g, colors.b);
+        }
+        else if (reqmode == "fade") {
+            servermode = "solid";
+            console.log("[INFO] Fading to " + "[" + req.body.r + " " + req.body.g + " " + req.body.b + "]");
+            var stepcount = 0;
+            while (colors.r != req.body.r || colors.g != req.body.g || colors.b != req.body.b) {
+                if (colors.r < req.body.r) {
+                    colors.r++;
+                }
+                if (colors.r > req.body.r) {
+                    colors.r--;
+                }
+                if (colors.g < req.body.g) {
+                    colors.g++;
+                }
+                if (colors.g > req.body.g) {
+                    colors.g--;
+                }
+                if (colors.b < req.body.b) {
+                    colors.b++;
+                }
+                if (colors.b > req.body.b) {
+                    colors.b--;
+                }
+                stepcount++;
+                sendColor(colors.r, colors.g, colors.b);
+            }
+            console.log("[INFO] Fade took " + stepcount + " steps");
+        }
+        else if (mode == "pulse") {
+        }
+        else if (mode == "blink") {
+        }
+        else if (mode == "disco") {
+        }
+        res.end();
+    }
+    else {
+        console.log("[INFO] Received unauthorized /status/ request");
+        res.statusCode = 401; // Is this working?
+        res.end("Authentication required");
+    }
 });
-
 function sendColor(newR, newG, newB) {
-	serial.write("$" + newR + "#" + newG + "#" + newB + "#", function(err, results) {
-		if (err) { console.log("[ERROR] " + err + ": " + results); }
-	});
+    serial.write("$" + newR + "#" + newG + "#" + newB + "#", function (err, results) {
+        if (err) {
+            console.log("[ERROR] " + err + ": " + results);
+        }
+    });
 }
